00a41df7fa748944127d2094c67992681ae647f1
vitor laguardia <vitor.laguardia@hotmail.com>
2024-04-22 18:39:57 -0300

    used routes.js in index.js server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
+app.use(routes);
 
-app.get("/", (request, response) => {
-  response.send("Hello Word");
-});
 app.listen(3000, console.log("Server started at http://localhost:3000"));

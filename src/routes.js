1cf6c6c361c792272f969f90fb3cfbcaa2082287
vitor laguardia <vitor.laguardia@hotmail.com>
2024-04-24 09:06:12 -0300

    fixed eslint problems

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-const { Router } = require("express");
-const ContactController = require("./app/controllers/ContactController");
+const { Router } = require('express');
+const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get("/contacts", ContactController.index);
+router.get('/contacts', ContactController.index);
 
 module.exports = router;

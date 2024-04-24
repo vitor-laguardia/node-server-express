cf1ccdebd6d831de040999aaf7ea7f155ace22e0
vitor laguardia <vitor.laguardia@hotmail.com>
2024-04-24 09:17:32 -0300

    fixed eslint problems in contactController

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -2,23 +2,27 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response) {
-    //list all registers
+    // list all registers
     const contacts = await ContactsRepository.findAll();
 
     response.json(contacts);
   }
+
   show() {
-    //get one register
+    // get one register
   }
+
   store() {
     // create new register
   }
+
   update() {
-    //edit a register
+    // edit a register
   }
+
   delete() {
-    //delete a register
+    // delete a register
   }
 }
-//D.P: Singleton --> 1 unic instance of this class is saved in cache.
+// D.P: Singleton --> 1 unic instance of this class is saved in cache.
 module.exports = new ContactController();

7397ea9852c5ac7ac6f64bc2056ef06c58641bbe
vitor laguardia <vitor.laguardia@hotmail.com>
2024-04-24 10:00:01 -0300

    implemented show method in contact controller

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -2,14 +2,20 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response) {
-    // list all registers
     const contacts = await ContactsRepository.findAll();
 
     response.json(contacts);
   }
 
-  show() {
-    // get one register
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+    if (!contact) {
+      // 404: not found
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
   store() {

f80e8fe0280c918bbd5af7870137432a9ce3df0e
vitor laguardia <vitor.laguardia@hotmail.com>
2024-04-29 18:08:22 -0300

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
     'consistent-return': 'off',
     camelcase: 'off',
     'object-curly-newline': 'off',
+    'no-confusing-arrow': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
   },
 };

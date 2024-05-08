badd03b852ab6718bfd65932a36d80737bf080ed
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-08 12:13:59 -0300

    refactores delete method in contacts repository to interact with db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,6 @@
 const { request } = require('express');
-const { v4 } = require('uuid');
 const db = require('../../database');
 
-let contacts = [
-  {
-    id: v4(),
-    name: 'vitor',
-    email: 'vitor@mail.com',
-    phone: '123456789321',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'leo',
-    email: 'leo@mail.com',
-    phone: '12343456789321',
-    category_id: v4(),
-  },
-];
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
@@ -39,11 +22,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`DELETE FROM contacts WHERE id = $1`, [id]);
+    return deleteOp;
   }
 
   async create(name, phone, email, category_id) {

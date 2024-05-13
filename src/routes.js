aef168e86a181c06abb8bc5bffc5633ba7653492
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-13 02:18:00 -0300

    created update route for categories

--- src/routes.js
+++ src/routes.js
@@ -12,5 +12,6 @@ router.put('/contacts/:id', ContactController.update);
 router.get('/categories', CategoryController.index);
 router.post('/categories', CategoryController.store);
 router.get('/categories/:id', CategoryController.show);
+router.post('/categories/:id', CategoryController.update);
 
 module.exports = router;

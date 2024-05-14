403b7bb77705aa71689ac7c659c100acbe4743e0
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-14 09:03:34 -0300

    created error midlaware handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+// this order is important
+require('express-async-errors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('###Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, console.log('Server started at http://localhost:3000'));
